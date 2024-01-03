2a4990b2b5f331e2bd2e6a94e899d37e1525dc02
-Miracle <73531883+Merck-web@users.noreply.github.com>
2024-01-03 16:21:29 +0500

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,19 @@
 <template>
-  <p>header</p>
+  <header
+      class="row align-items-center justify-content-between flex-wrap card-header"
+  >
+    <img
+        class="logo"
+        src="img/logo.png"
+        alt="logo"
+    >
+
+    <b-btn
+        variant="primary"
+    >
+      Добавить новый рецепт
+    </b-btn>
+  </header>
 </template>
 
 <script>
@@ -9,6 +23,15 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style
+    scoped
+    lang="scss"
+>
+.logo {
+  width: 50px;
+  height: 50px;
+}
+.card-header {
+  border: 1px solid rgba(0,0,0,.125);
+}
 </style>
